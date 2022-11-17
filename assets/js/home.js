a45712c459fb3681bc014885762542b0856c4e2b
Douglas Batista <dougbatista.ti@gmail.com>
2022-11-16 21:04:01 -0300

    adicionado javascript

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,36 +4,53 @@ array_pointers.forEach( (each) => {
     each.addEventListener('click', (e) => {
         e.preventDefault();
 
-        updateSlide();
+        updateSlideshow(e.target.getAttribute('data-pointer'));
     });
 });
 
-function updateSlide(){
+function updateSlideshow (param){
 
+    console.log(param)
+    if(param !== undefined){
+        // document.querySelector('.depoiments--pointers a.active').classList.remove('active'); 
+        if(param == 0){
+            document.querySelector(".depoiments--area").style.marginLeft = '-100vw';
+            slideCurrent++;
+            document.querySelector(`.depoiments--pointers a[data-pointer="${param}"]`).classList.add('active');
 
-
+        }else if(param == 1){
+            document.querySelector(".depoiments--area").style.marginLeft = '-200vw';
+            slideCurrent++;
+            document.querySelector(`.depoiments--pointers a[data-pointer="${param}"]`).classList.add('active');
+        }else {
+            document.querySelector(".depoiments--area").style.marginLeft = '0';
+            slideCurrent = 0;
+            document.querySelector(`.depoiments--pointers a[data-pointer="${param}"]`).classList.add('active');
+        }
+    }
 }
-
 function slideshow () {    
     
-    let slideCurrent = 0;
+    let slideCurrent = 0; 
 
     setInterval( () => {
         let pointer = document.querySelector('.depoiments--pointers a.active').getAttribute('data-pointer');
-        // let sliders = document.querySelector('.depoiments--pointers a.active').classList.remove('active');
+        document.querySelector('.depoiments--pointers a.active').classList.remove('active');        
 
         if(pointer){
             if(slideCurrent == 0){
                 document.querySelector(".depoiments--area").style.marginLeft = '-100vw';
                 slideCurrent++;
-                console.log(slideCurrent)              
+                document.querySelector(`.depoiments--pointers a[data-pointer="${slideCurrent}"]`).classList.add('active');
+
             }else if(slideCurrent == 1){
                 document.querySelector(".depoiments--area").style.marginLeft = '-200vw';
                 slideCurrent++;
-                console.log(slideCurrent) 
+                document.querySelector(`.depoiments--pointers a[data-pointer="${slideCurrent}"]`).classList.add('active');
             }else {
                 document.querySelector(".depoiments--area").style.marginLeft = '0';
                 slideCurrent = 0;
+                document.querySelector(`.depoiments--pointers a[data-pointer="${slideCurrent}"]`).classList.add('active');
             }
         }
     }, 4000);
